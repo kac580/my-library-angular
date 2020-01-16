e9c5a5ae801cd652924e47e08290782415b51da6
Kyra Buchmann <kac580@outlook.com>
2020-01-15 20:19:29 -0600

    modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from './book';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-library';
+
+  toRead =[
+    new Book("A Memoir of Migraine Survival", "Danielle Newport Fancher", 2018),
+    new Book("Migraine", "Oliver Sacks",1999),
+  ]
 }
+
--- src/app/books-list/books-list.component.ts
+++ src/app/books-list/books-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Book } from '../book';
 
 @Component({
   selector: 'app-books-list',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksListComponent implements OnInit {
 
+  @Input() books: Book[];
+  @Input() name: string;
+
+  newTitle: string;
+  newAuthor: string;
+  newYear: number;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  details(book: Book){
+    alert(`details for ${book.title}`)
+  }
+
+  addMovie() {
+    let book = new Book (this.newTitle, this.newAuthor, this.newYear);
+    this.books.push(book);
+  }
 }
